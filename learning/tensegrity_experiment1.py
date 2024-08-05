2237f413901241858765dc68d33724e9f248b83a
Dong-geon24 <rkdtjtneh@kyonggi.ac.kr>
2024-08-05 22:29:08 +0900

    [modified][learning] x축에 대한 움직임으로 제한

--- learning/tensegrity_experiment1.py
+++ learning/tensegrity_experiment1.py
@@ -127,50 +127,102 @@ class TensegrityEnv(MujocoEnv, utils.EzPickle):
             )
         )
 
+    # def step(self, action):
+    #     xy_position_before = mass_center(self.model, self.data)
+    #     self.do_simulation(action, self.frame_skip)
+    #     xy_position_after = mass_center(self.model, self.data)
+
+    #     xy_velocity = (xy_position_after - xy_position_before) / self.dt
+    #     # x_velocity, y_velocity = xy_velocity
+
+    #     # ctrl_cost = self.control_cost(action)
+
+        
+    #     # 이동거리 보상
+    #     distance_traveled = np.linalg.norm(xy_position_after - xy_position_before)
+    #     distance_reward = self._distance_reward_weight*distance_traveled
+
+    #     # 총 이동 거리 보상 (시작점으로부터의 거리)
+    #     total_distance = np.linalg.norm(xy_position_after - self.initial_xy_position)
+    #     total_distance_reward = self._total_distance_reward_weight * total_distance
+        
+    #     # 속도 보상
+    #     velocity_reward = self._velocity_reward_weight * np.linalg.norm(xy_velocity)
+
+    #     healthy_reward = self.healthy_reward
+
+    #     # 최종 보상 계산
+    #     reward = distance_reward + total_distance_reward + velocity_reward + healthy_reward
+
+    #       # 간단한 종료 조건 (너무 낮거나 높아지면 종료)
+    #     terminated = bool(self.data.qpos[2] < 0.1 or self.data.qpos[2] > 1.0)
+
+    #     observation = self._get_obs()
+    #     info = {
+    #     "reward_distance": distance_reward,
+    #     "reward_total_distance": total_distance_reward,
+    #     "reward_velocity": velocity_reward,
+    #     # "reward_rotation": rotation_reward,
+    #     "x_position": xy_position_after[0],
+    #     "y_position": xy_position_after[1],
+    #     "height": self.data.qpos[2],
+    #     "distance_traveled": distance_traveled,
+    #     "total_distance": total_distance,
+    #     }
+
+    #     if self.render_mode == "human":
+    #         self.render()
+    #     return observation, reward, terminated, False, info
+    
     def step(self, action):
         xy_position_before = mass_center(self.model, self.data)
         self.do_simulation(action, self.frame_skip)
         xy_position_after = mass_center(self.model, self.data)
 
         xy_velocity = (xy_position_after - xy_position_before) / self.dt
-        # x_velocity, y_velocity = xy_velocity
 
-        # ctrl_cost = self.control_cost(action)
+        # 현재 스텝에서의 x축 이동 거리 계산
+        x_distance_traveled = abs(xy_position_after[0] - xy_position_before[0])
+        distance_reward = self._distance_reward_weight * x_distance_traveled
 
-        
-        # 이동거리 보상
-        distance_traveled = np.linalg.norm(xy_position_after - xy_position_before)
-        distance_reward = self._distance_reward_weight*distance_traveled
+        # 초기 위치에서의 x축 총 이동 거리 계산
+        total_x_distance = abs(xy_position_after[0] - self.initial_xy_position[0])
+        total_distance_reward = self._total_distance_reward_weight * total_x_distance
 
-        # 총 이동 거리 보상 (시작점으로부터의 거리)
-        total_distance = np.linalg.norm(xy_position_after - self.initial_xy_position)
-        total_distance_reward = self._total_distance_reward_weight * total_distance
-        
-        # 속도 보상
-        velocity_reward = self._velocity_reward_weight * np.linalg.norm(xy_velocity)
+        # 초기 위치에서부터의 x축 거리 증가 보상
+        distance_increase = total_x_distance - self.previous_distance_traveled
+        distance_increase_reward = self._distance_increase_reward_weight * distance_increase
+        self.previous_distance_traveled = total_x_distance
+
+        # x축 속도 보상 계산
+        velocity_reward = self._velocity_reward_weight * abs(xy_velocity[0])
+
+        # 자세 유지 보상 추가
+        healthy_reward = self.healthy_reward
 
         # 최종 보상 계산
-        reward = distance_reward + total_distance_reward + velocity_reward
+        reward = distance_reward + total_distance_reward + velocity_reward + distance_increase_reward + healthy_reward
 
-          # 간단한 종료 조건 (너무 낮거나 높아지면 종료)
         terminated = bool(self.data.qpos[2] < 0.1 or self.data.qpos[2] > 1.0)
 
         observation = self._get_obs()
         info = {
-        "reward_distance": distance_reward,
-        "reward_total_distance": total_distance_reward,
-        "reward_velocity": velocity_reward,
-        # "reward_rotation": rotation_reward,
-        "x_position": xy_position_after[0],
-        "y_position": xy_position_after[1],
-        "height": self.data.qpos[2],
-        "distance_traveled": distance_traveled,
-        "total_distance": total_distance,
+            "reward_distance": distance_reward,
+            "reward_total_distance": total_distance_reward,
+            "reward_velocity": velocity_reward,
+            "reward_healthy": healthy_reward,
+            "reward_distance_increase": distance_increase_reward,
+            "x_position": xy_position_after[0],
+            "y_position": xy_position_after[1],
+            "height": self.data.qpos[2],
+            "distance_traveled": x_distance_traveled,
+            "total_distance": total_x_distance,
         }
 
         if self.render_mode == "human":
             self.render()
         return observation, reward, terminated, False, info
+
 
     def reset_model(self):
         noise_low = -self._reset_noise_scale
